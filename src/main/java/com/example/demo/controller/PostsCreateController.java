ab4d83975e6960c977a1723c6ae3eadf8014ad1b
3l0i_Kot <arsenghj@mail.ru>
2025-03-24 01:41:32 +0500

    Post creation

---
+++ src/main/java/com/example/demo/controller/PostsCreateController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Post;
+import com.example.demo.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+public class PostsCreateController {
+
+    @Autowired
+    PostService postService;
+
+    @RequestMapping(path = "/new", method = RequestMethod.GET)
+    public String create(Model model) {
+        model.addAttribute("appName", "Моё супер приложение");
+
+        return "create";
+    }
+
+    @RequestMapping(path = "/new", method = RequestMethod.POST)
+    public String doCreate(@ModelAttribute("text") String text) {
+        postService.create(text);
+        return "redirect:/";
+    }
+
+}
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -9,13 +9,22 @@ import java.util.List;
 
 @Service
 public class PostService {
+
+    private final List<Post> posts;
+
+    // Блок инициализации для первоначального наполнения списка
+    {
+        posts = new ArrayList<>();
+        posts.add(new Post("Первый пост", new Date()));
+        posts.add(new Post("Второй пост", new Date()));
+        posts.add(new Post("Третий пост", new Date()));
+    }
+
     public List<Post> listAllPosts() {
-        List<Post> posts = new ArrayList<>();
-
-        posts.add(new Post("Первый пост.", new Date()));
-        posts.add(new Post("Второй пост.", new Date()));
-        posts.add(new Post("Третий пост.", new Date()));
-
         return posts;
+    }
+
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
