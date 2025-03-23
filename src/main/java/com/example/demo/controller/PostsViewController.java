acac1ee89a2773e8057c310aa91793f708b87967
3l0i_Kot <arsenghj@mail.ru>
2025-03-24 01:02:20 +0500

    List of posts

--- src/main/java/com/example/demo/controller/PostsViewController.java
+++ src/main/java/com/example/demo/controller/PostsViewController.java
@@ -1,15 +1,27 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Post;
+import com.example.demo.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 public class PostsViewController {
+
+    @Autowired
+    PostService postService;
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String list(Model model) {
         model.addAttribute("appName", "Моё супер приложение");
+
+        List<Post> posts = postService.listAllPosts();
+        model.addAttribute("posts", posts);
+
         return "list";
     }
 
