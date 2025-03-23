5e4e0ab8edcbdc6226b653ef6f73f3920fbbe389
3l0i_Kot <arsenghj@mail.ru>
2025-03-24 01:26:45 +0500

    Bootstrap

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,13 +1,17 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
 
     private String text;
     private Integer likes;
+    private Date creationDate;
 
-    public Post(String text) {
+    public Post(String text, Date creationDate) {
         this.text = text;
-        this.likes = 0; // По умолчанию лайков нет
+        this.creationDate = creationDate;
+        this.likes = 0;
     }
 
     public String getText() {
@@ -16,5 +20,9 @@ public class Post {
 
     public Integer getLikes() {
         return likes;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
     }
 }
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -4,6 +4,7 @@ import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -11,9 +12,9 @@ public class PostService {
     public List<Post> listAllPosts() {
         List<Post> posts = new ArrayList<>();
 
-        posts.add(new Post("Первый пост."));
-        posts.add(new Post("Второй пост."));
-        posts.add(new Post("Третий пост."));
+        posts.add(new Post("Первый пост.", new Date()));
+        posts.add(new Post("Второй пост.", new Date()));
+        posts.add(new Post("Третий пост.", new Date()));
 
         return posts;
     }
